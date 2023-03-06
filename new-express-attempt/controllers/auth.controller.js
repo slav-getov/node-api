5df4d3da070cf90cff0909ce32af7708fbbebfdd
Preslav Getov <preslav.getov@yara.com>
2023-03-06 09:27:09 +0200

    attempting verify log

--- new-express-attempt/controllers/auth.controller.js
+++ new-express-attempt/controllers/auth.controller.js
@@ -1,7 +1,12 @@
 const User = require('../models/user.model.js')
+//validators
 const { body, validationResult } = require('express-validator');
-const bcrypt = require('bcrypt')
 
+//outside libs
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+//actual implementation 
 const login = async (req,res)=>{
     try {
         const {id} = req.params
@@ -9,7 +14,18 @@ const login = async (req,res)=>{
         const user = await User.findByPk(id);
         if(!password || !user) return res.status(400).json({message: "id and pass required!"})
         const match = await bcrypt.compare(password, user.password)
-        console.log('matched!!!')
+        if(match){
+            const accessT = jwt.sign(
+                {"email": user.email},
+                process.env.ACCESS_TOKEN_SECRET,
+                {expiresIn: '10m'}
+            )
+            res.json({accessToken: accessT})
+        }else{
+            //obv not auth-ed
+            res.sendStatus(401)
+        }
+        
         //res.send(JSON.stringify(user, null, '<pre>'));
     } catch (err) {
         console.log(err);
---
+++ new-express-attempt/helpers/verifyAuth.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken')
+require('dotenv').config
+
+const verifyAuth = (req,res,next)=>{
+    const authHeader = req.headers['authorization']
+    if(!authHeader) return res.sendStatus(401)
+    console.log(authHeader)
+    //test???
+
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET, (err, decodedValue) =>{
+        if(err) return res.sendStatus(403) //invalid/tampered with
+        req.user = decodedValue.email
+        next()
+    })
+    
+}
+
+module.exports = verifyAuth
