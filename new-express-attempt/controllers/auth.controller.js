2c4c5cb4eb0d2aeb6efa5ac39444648f138c7dd8
Preslav Getov <preslav.getov@yara.com>
2023-03-06 08:13:59 +0200

    comparer function works

--- new-express-attempt/controllers/auth.controller.js
+++ new-express-attempt/controllers/auth.controller.js
@@ -1,12 +1,16 @@
 const User = require('../models/user.model.js')
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcrypt')
 
 const login = async (req,res)=>{
     try {
         const {id} = req.params
+        const {password} = req.body
         const user = await User.findByPk(id);
-        console.log(user.password)
-        res.send(JSON.stringify(user, null, '<pre>'));
+        if(!password || !user) return res.status(400).json({message: "id and pass required!"})
+        const match = await bcrypt.compare(password, user.password)
+        console.log('matched!!!')
+        //res.send(JSON.stringify(user, null, '<pre>'));
     } catch (err) {
         console.log(err);
     }
--- new-express-attempt/routes/user.routes.js
+++ new-express-attempt/routes/user.routes.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const {createUser} = require('../controllers/user.controller.js')
 const {login} = require('../controllers/auth.controller.js')
 
-router.get('/login-:id-:pswd' , login)
+router.post('/login-:id', login)
 router.post('/create',createUser)
 
 module.exports = router
